73a86bb281c8497fa7227e786eabe922203c83ee
eb <eb@eb2.co>
2023-12-05 14:31:53 -0700

    Day 05 refactoring

--- README.md
+++ README.md
@@ -21,11 +21,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `74.0µs` | `606.6µs` |
-| [Day 2](./src/bin/02.rs) | `31.7µs` | `31.0µs` |
-| [Day 3](./src/bin/03.rs) | `43.7µs` | `155.2µs` |
-| [Day 4](./src/bin/04.rs) | `150.5µs` | `157.8µs` |
-| [Day 5](./src/bin/05.rs) | `30.4µs` | `128.3s` |
+| [Day 1](./src/bin/01.rs) | `119.5µs` | `652.4µs` |
+| [Day 2](./src/bin/02.rs) | `35.5µs` | `35.2µs` |
+| [Day 3](./src/bin/03.rs) | `56.1µs` | `227.6µs` |
+| [Day 4](./src/bin/04.rs) | `225.5µs` | `229.0µs` |
+| [Day 5](./src/bin/05.rs) | `51.3µs` | `6.7s` |
 
-**Total: 128301.28ms**
+**Total: 6701.63ms**
 <!--- benchmarking table --->
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(5);
 
 #[derive(Debug)]
@@ -40,55 +42,72 @@ fn parse_seeds(input: &str) -> Vec<u64> {
     advent_of_code::parse_space_separated::<u64>(&input[6..])
 }
 
-fn process_seed(seed: u64, almanac_maps: &Vec<Vec<AlmanacMap>>) -> u64 {
-    let mut seed = seed;
+fn parse_seed_ranges(input: &str) -> Vec<(u64, u64)> {
+    let seed_ranges = advent_of_code::parse_space_separated::<u64>(&input[6..]);
 
-    for almanac_map in almanac_maps {
+    seed_ranges
+        .chunks(2)
+        .map(|chunk| (chunk[0], chunk[1]))
+        .sorted_by(|a, b| a.0.cmp(&b.0))
+        .collect()
+}
+
+fn process_seed_to_location(seed: u64, almanac_maps: &Vec<Vec<AlmanacMap>>) -> u64 {
+    almanac_maps.iter().fold(seed, |seed_test, almanac_map| {
         for map in almanac_map {
-            if seed >= map.source_range_start && seed < map.source_range_start + map.range_length {
-                seed = map.dest_range_start + (seed - map.source_range_start);
-                break;
+            if seed_test >= map.source_range_start
+                && seed_test < map.source_range_start + map.range_length
+            {
+                return map.dest_range_start + (seed_test - map.source_range_start);
             }
         }
-    }
 
-    seed
+        seed_test
+    })
+}
+
+fn process_location_to_seed(location: u64, almanac_maps: &Vec<Vec<AlmanacMap>>) -> u64 {
+    almanac_maps
+        .iter()
+        .rev()
+        .fold(location, |location, almanac_map| {
+            for map in almanac_map {
+                if location >= map.dest_range_start
+                    && location < map.dest_range_start + map.range_length
+                {
+                    return map.source_range_start + (location - map.dest_range_start);
+                }
+            }
+
+            location
+        })
+}
+
+fn seed_in_ranges(seed: u64, seed_ranges: &Vec<(u64, u64)>) -> bool {
+    seed_ranges
+        .iter()
+        .any(|(start, length)| seed >= *start && seed <= *start + *length)
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let seeds = parse_seeds(input.lines().next().unwrap());
     let almanac_maps = parse_almanac_maps(input);
 
-    let mut min_location = u64::MAX;
-
-    for mut seed in seeds {
-        seed = process_seed(seed, &almanac_maps);
-
-        if seed < min_location {
-            min_location = seed;
-        }
-    }
-
-    Some(min_location)
+    seeds
+        .iter()
+        .map(|seed| process_seed_to_location(*seed, &almanac_maps))
+        .min()
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let seeds = parse_seeds(input.lines().next().unwrap());
+    let seed_ranges = parse_seed_ranges(input.lines().next().unwrap());
     let almanac_maps = parse_almanac_maps(input);
 
-    let mut min_location = u64::MAX;
-
-    for chunk in seeds.chunks(2) {
-        for mut seed in chunk[0]..(chunk[0] + chunk[1]) {
-            seed = process_seed(seed, &almanac_maps);
-
-            if seed < min_location {
-                min_location = seed;
-            }
-        }
-    }
-
-    Some(min_location)
+    (0..u64::MAX)
+        .map(|location| (location, process_location_to_seed(location, &almanac_maps)))
+        .filter(|(_, seed)| seed_in_ranges(*seed, &seed_ranges))
+        .map(|(location, _)| location)
+        .next()
 }
 
 #[cfg(test)]
