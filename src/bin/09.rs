89ab19fc3de2b234ce04de76de3994cb112e6f5f
eb <eb@eb2.co>
2023-12-09 05:26:04 -0700

    Day 09 refactor

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 advent_of_code::solution!(9);
 
 fn parse_histories(input: &str) -> Vec<Vec<i32>> {
@@ -10,64 +8,61 @@ fn parse_histories(input: &str) -> Vec<Vec<i32>> {
 }
 
 fn process_history(history: &[i32]) -> Option<i32> {
-    let mut sequences = VecDeque::new();
-    let mut current_sequence = VecDeque::new();
-    let mut previous_sequence = history.to_vec();
-    sequences.push_back(VecDeque::from(history.to_vec()));
+    let mut sequences: Vec<Vec<i32>> = Vec::new();
+    let mut current_sequence = Vec::new();
+
+    sequences.push(history.to_vec());
 
     loop {
-        for slice in previous_sequence.windows(2) {
+        for slice in sequences.last().unwrap().windows(2) {
             let (a, b) = (slice[0], slice[1]);
 
-            current_sequence.push_back(b - a);
+            current_sequence.push(b - a);
         }
 
-        sequences.push_back(current_sequence.clone());
+        sequences.push(current_sequence.clone());
 
         if current_sequence.iter().sum::<i32>() == 0 {
             break;
         }
 
-        previous_sequence = current_sequence.iter().copied().collect();
         current_sequence.clear();
     }
 
     let mut last: i32 = 0;
 
     for sequence in sequences.iter().rev() {
-        last += sequence.back().unwrap();
+        last += sequence.last().unwrap();
     }
 
     Some(last)
 }
 
 fn process_history2(history: &[i32]) -> Option<i32> {
-    let mut sequences = VecDeque::new();
-    let mut current_sequence = VecDeque::new();
-    let mut previous_sequence = history.to_vec();
-    sequences.push_back(VecDeque::from(history.to_vec()));
+    let mut sequences: Vec<Vec<i32>> = Vec::new();
+    let mut current_sequence = Vec::new();
+    sequences.push(history.to_vec());
 
     loop {
-        for slice in previous_sequence.windows(2) {
+        for slice in sequences.last().unwrap().windows(2) {
             let (a, b) = (slice[0], slice[1]);
 
-            current_sequence.push_back(b - a);
+            current_sequence.push(b - a);
         }
 
-        sequences.push_back(current_sequence.clone());
+        sequences.push(current_sequence.clone());
 
         if current_sequence.iter().sum::<i32>() == 0 {
             break;
         }
 
-        previous_sequence = current_sequence.iter().copied().collect();
         current_sequence.clear();
     }
 
     let mut first: i32 = 0;
 
     for sequence in sequences.iter().rev() {
-        first = sequence.front().unwrap() - first;
+        first = sequence.first().unwrap() - first;
     }
 
     Some(first)
