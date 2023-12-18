74967b28d19d3b6322c61999266b88f403bb5aaa
eb <eb@eb2.co>
2023-12-18 16:17:54 -0700

    Day 17 refactor

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,143 +1,53 @@
-use std::collections::{HashMap, VecDeque};
-
-use advent_of_code::Point2D;
+use std::collections::{BinaryHeap, HashMap};
 
 advent_of_code::solution!(17);
 
-fn parse_edge_weights(input: &str) -> HashMap<Point2D, u32> {
-    let mut edge_weights: HashMap<Point2D, u32> = HashMap::new();
-
-    input.lines().enumerate().for_each(|(y, line)| {
-        line.chars().enumerate().for_each(|(x, b)| {
-            let point = Point2D::new(x as isize, y as isize);
-            let weight = b.to_digit(10).unwrap();
-
-            edge_weights.insert(point, weight);
-        });
-    });
-
-    edge_weights
-}
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let edge_weights = parse_edge_weights(input);
-    let mut distances: HashMap<(Point2D, Point2D, u32), u32> = HashMap::new();
-    let mut visit: VecDeque<(Point2D, Point2D, u32)> = VecDeque::new();
-    visit.push_back((Point2D::new(0, 0), Point2D::new(1, 0), 0));
-
-    let lines = input.lines().collect::<Vec<&str>>();
-    let width = lines[0].len();
-    let height = lines.len();
-
-    distances.insert((Point2D::new(0, 0), Point2D::new(1, 0), 0), 0);
-
-    while !visit.is_empty() {
-        let (cur_pos, cur_dir, counter) = visit.pop_front().unwrap();
-        let current_distance = *distances
-            .get(&(cur_pos, cur_dir, counter))
-            .unwrap_or(&u32::MAX);
-
-        for step in [
-            Point2D::new(-1, 0),
-            Point2D::new(1, 0),
-            Point2D::new(0, -1),
-            Point2D::new(0, 1),
-        ] {
-            let dir_to_previous = cur_dir.mul_scalar(-1);
-            if step == dir_to_previous || (counter == 3 && step == cur_dir) {
+fn dijkstra(grid: &[&[u8]], minstep: isize, maxstep: isize) -> i64 {
+    let mut dists = HashMap::new();
+    let mut q = BinaryHeap::from_iter([(0, (0, 0, (0, 0)))]);
+    while let Some((cost, (r, c, d))) = q.pop() {
+        if (r, c) == (grid.len() - 1, grid[0].len() - 1) {
+            return -cost;
+        }
+        if dists.get(&(r, c, d)).is_some_and(|&c| -cost > c) {
+            continue;
+        }
+        for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            if d == (dr, dc) || d == (-dr, -dc) {
                 continue;
             }
-
-            let next_pos = cur_pos.add(&step);
-            let next_dir = step;
-            let next_counter = if step == cur_dir { counter + 1 } else { 1 };
-
-            if edge_weights.contains_key(&next_pos) {
-                let next_distance = current_distance + edge_weights.get(&next_pos).unwrap();
-
-                if next_distance
-                    < *distances
-                        .get(&(next_pos, next_dir, next_counter))
-                        .unwrap_or(&u32::MAX)
-                {
-                    distances.insert((next_pos, next_dir, next_counter), next_distance);
-                    visit.push_back((next_pos, next_dir, next_counter));
+            let mut next_cost = -cost;
+            for dist in 1..=maxstep {
+                let rr = (r as isize + dr * dist) as usize;
+                let cc = (c as isize + dc * dist) as usize;
+                if rr >= grid.len() || cc >= grid[0].len() {
+                    continue;
+                }
+                next_cost += (grid[rr][cc] - b'0') as i64;
+                if dist < minstep {
+                    continue;
+                }
+                let key = (rr, cc, (dr, dc));
+                if next_cost < *dists.get(&key).unwrap_or(&i64::MAX) {
+                    dists.insert(key, next_cost);
+                    q.push((-next_cost, key));
                 }
             }
         }
     }
-
-    let (_, distance) = distances
-        .iter()
-        .filter(|((pos, _, _), _)| *pos == Point2D::new(width as isize - 1, height as isize - 1))
-        .min_by_key(|(_, distance)| **distance)
-        .unwrap();
-
-    Some(*distance)
+    unreachable!()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let edge_weights = parse_edge_weights(input);
-    let mut distances: HashMap<(Point2D, Point2D, u32), u32> = HashMap::new();
-    let mut visit: VecDeque<(Point2D, Point2D, u32)> = VecDeque::new();
-    visit.push_back((Point2D::new(0, 0), Point2D::new(1, 0), 0));
+pub fn part_one(input: &str) -> Option<i64> {
+    let grid = input.lines().map(str::as_bytes).collect::<Vec<_>>();
 
-    let lines = input.lines().collect::<Vec<&str>>();
-    let width = lines[0].len();
-    let height = lines.len();
+    Some(dijkstra(&grid, 1, 3))
+}
 
-    distances.insert((Point2D::new(0, 0), Point2D::new(1, 0), 0), 0);
+pub fn part_two(input: &str) -> Option<i64> {
+    let grid = input.lines().map(str::as_bytes).collect::<Vec<_>>();
 
-    while !visit.is_empty() {
-        let (cur_pos, cur_dir, counter) = visit.pop_front().unwrap();
-        let current_distance = *distances
-            .get(&(cur_pos, cur_dir, counter))
-            .unwrap_or(&u32::MAX);
-
-        for step in [
-            Point2D::new(-1, 0),
-            Point2D::new(1, 0),
-            Point2D::new(0, -1),
-            Point2D::new(0, 1),
-        ] {
-            let dir_to_previous = cur_dir.mul_scalar(-1);
-            if step == dir_to_previous
-                || (step == cur_dir && counter == 10)
-                || (step != cur_dir && counter < 4)
-            {
-                continue;
-            }
-
-            let next_pos = cur_pos.add(&step);
-            let next_dir = step;
-            let next_counter = if step == cur_dir { counter + 1 } else { 1 };
-
-            // dbg!(next_pos, next_dir, next_counter);
-
-            if edge_weights.contains_key(&next_pos) {
-                let next_distance = current_distance + edge_weights.get(&next_pos).unwrap();
-
-                if next_distance
-                    < *distances
-                        .get(&(next_pos, next_dir, next_counter))
-                        .unwrap_or(&u32::MAX)
-                {
-                    distances.insert((next_pos, next_dir, next_counter), next_distance);
-                    visit.push_back((next_pos, next_dir, next_counter));
-                }
-            }
-        }
-    }
-
-    let (_, distance) = distances
-        .iter()
-        .filter(|((pos, _, counter), _)| {
-            *pos == Point2D::new(width as isize - 1, height as isize - 1) && *counter >= 4
-        })
-        .min_by_key(|(_, distance)| **distance)
-        .unwrap();
-
-    Some(*distance)
+    Some(dijkstra(&grid, 4, 10))
 }
 
 #[cfg(test)]
