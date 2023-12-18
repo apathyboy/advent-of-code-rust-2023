07639571c580bc9f50001c005ff073c674d7f76a
Eric Barr <eb@eb2.co>
2023-12-17 17:23:39 -0700

    Part 1 solution

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -21,39 +21,21 @@ fn parse_edge_weights(input: &str) -> HashMap<Point2D, u32> {
 
 pub fn part_one(input: &str) -> Option<u32> {
     let edge_weights = parse_edge_weights(input);
-    let mut distances: HashMap<(Point2D, Point2D), u32> = HashMap::new();
+    let mut distances: HashMap<(Point2D, Point2D, u32), u32> = HashMap::new();
     let mut visit: VecDeque<(Point2D, Point2D, u32)> = VecDeque::new();
-    visit.push_back((Point2D::new(0, 0), Point2D::new(1, 0), 1));
+    visit.push_back((Point2D::new(0, 0), Point2D::new(1, 0), 0));
 
     let lines = input.lines().collect::<Vec<&str>>();
     let width = lines[0].len();
     let height = lines.len();
 
-    // implement dijkstra's algorithm
-    // 1. set the distance to the starting node to 0
-    // 2. set the distance to all other nodes to infinity
-    for (point, _) in edge_weights.iter() {
-        if *point == Point2D::new(0, 0) {
-            distances.insert((*point, Point2D::new(1, 0)), 0);
-            distances.insert((*point, Point2D::new(-1, 0)), std::u32::MAX);
-            distances.insert((*point, Point2D::new(0, -1)), std::u32::MAX);
-            distances.insert((*point, Point2D::new(0, 1)), std::u32::MAX);
-        } else {
-            distances.insert((*point, Point2D::new(-1, 0)), std::u32::MAX);
-            distances.insert((*point, Point2D::new(1, 0)), std::u32::MAX);
-            distances.insert((*point, Point2D::new(0, -1)), std::u32::MAX);
-            distances.insert((*point, Point2D::new(0, 1)), std::u32::MAX);
-        }
-    }
+    distances.insert((Point2D::new(0, 0), Point2D::new(1, 0), 0), 0);
 
     while !visit.is_empty() {
-        // 3. visit the next unvisited node
-        // 4. for the current node, consider all of its unvisited neighbors and calculate their tentative distances
-        // 5. when we are done considering all of the neighbors of the current node, mark the current node as visited
-        // 6. if the destination node has been marked visited or if the smallest tentative distance among the nodes in the unvisited set is infinity, then stop. the algorithm has finished
-        // 7. otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node", and go back to step 3
         let (cur_pos, cur_dir, counter) = visit.pop_front().unwrap();
-        let current_distance = *distances.get(&(cur_pos, cur_dir)).unwrap();
+        let current_distance = *distances
+            .get(&(cur_pos, cur_dir, counter))
+            .unwrap_or(&u32::MAX);
 
         for step in [
             Point2D::new(-1, 0),
@@ -73,27 +55,22 @@ pub fn part_one(input: &str) -> Option<u32> {
             if edge_weights.contains_key(&next_pos) {
                 let next_distance = current_distance + edge_weights.get(&next_pos).unwrap();
 
-                if next_distance < *distances.get(&(next_pos, next_dir)).unwrap() {
-                    distances.insert((next_pos, next_dir), next_distance);
+                if next_distance
+                    < *distances
+                        .get(&(next_pos, next_dir, next_counter))
+                        .unwrap_or(&u32::MAX)
+                {
+                    distances.insert((next_pos, next_dir, next_counter), next_distance);
                     visit.push_back((next_pos, next_dir, next_counter));
                 }
             }
         }
-
-        //let (next_pos, next_dir) = visit.pop_front().unwrap();
-        //let current_distance = distances.get(&(next_pos, next_dir)).unwrap();
     }
 
     let (_, distance) = distances
         .iter()
-        .filter(|((pos, _), _)| *pos == Point2D::new(width as isize - 1, height as isize - 1))
-        .min_by_key(|(_, distance)| **distance)
+        .find(|((pos, _, _), _)| *pos == Point2D::new(width as isize - 1, height as isize - 1))
         .unwrap();
-
-    // 4. for the current node, consider all of its unvisited neighbors and calculate their tentative distances
-    // 5. when we are done considering all of the neighbors of the current node, mark the current node as visited
-    // 6. if the destination node has been marked visited or if the smallest tentative distance among the nodes in the unvisited set is infinity, then stop. the algorithm has finished
-    // 7. otherwise, select the unvisited node that is marked with the smallest tentative distance, set it as the new "current node", and go back to step 3
 
     Some(*distance)
 }
