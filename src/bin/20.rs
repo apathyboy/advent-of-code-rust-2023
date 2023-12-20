9e3727a356488433c64cff9b4f4b9b831b769e2d
eb <eb@eb2.co>
2023-12-20 10:35:49 -0700

    Day 20 refactor

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -6,51 +6,62 @@ use std::collections::{HashMap, VecDeque};
 advent_of_code::solution!(20);
 
 #[derive(Debug, PartialEq, Clone)]
-enum Pulse {
+enum PulseType {
     High,
     Low,
 }
 
+struct Pulse<'a> {
+    pulse_type: PulseType,
+    source: &'a str,
+    destination: &'a str,
+}
+
+impl<'a> Pulse<'a> {
+    fn new(pulse_type: PulseType, source: &'a str, destination: &'a str) -> Self {
+        Self {
+            pulse_type,
+            source,
+            destination,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
-enum CommsModule {
+enum CommsModule<'a> {
     FlipFlop {
         on_off: bool,
-        destinations: Vec<String>,
-        id: String,
+        destinations: Vec<&'a str>,
+        id: &'a str,
     },
     Conjunction {
-        inputs: HashMap<String, Pulse>,
-        destinations: Vec<String>,
-        id: String,
+        inputs: HashMap<&'a str, PulseType>,
+        destinations: Vec<&'a str>,
+        id: &'a str,
     },
     Broadcaster {
-        destinations: Vec<String>,
-        id: String,
+        destinations: Vec<&'a str>,
+        id: &'a str,
     },
 }
 
-impl CommsModule {
+impl<'a> CommsModule<'a> {
     fn has_destination(&self, id: &str) -> bool {
         match self {
-            Self::FlipFlop { destinations, .. } => destinations.contains(&id.to_string()),
-            Self::Conjunction { destinations, .. } => destinations.contains(&id.to_string()),
-            Self::Broadcaster { destinations, .. } => destinations.contains(&id.to_string()),
+            Self::FlipFlop { destinations, .. } => destinations.contains(&id),
+            Self::Conjunction { destinations, .. } => destinations.contains(&id),
+            Self::Broadcaster { destinations, .. } => destinations.contains(&id),
         }
     }
 
-    fn signal(
-        &mut self,
-        source: &str,
-        pulse_queue: &mut VecDeque<(String, String, Pulse)>,
-        pulse: Pulse,
-    ) {
+    fn signal(&mut self, pulse_queue: &mut VecDeque<Pulse<'a>>, pulse: &Pulse<'a>) {
         match self {
             Self::FlipFlop {
                 on_off,
                 destinations,
                 id,
             } => {
-                if pulse == Pulse::High {
+                if pulse.pulse_type == PulseType::High {
                     return;
                 }
 
@@ -59,12 +70,12 @@ impl CommsModule {
                 if *on_off {
                     for destination in destinations.iter() {
                         //println!("{} -high-> {}", id, destination);
-                        pulse_queue.push_back((id.clone(), destination.clone(), Pulse::High));
+                        pulse_queue.push_back(Pulse::new(PulseType::High, id, destination));
                     }
                 } else {
                     for destination in destinations.iter() {
                         //println!("{} -low-> {}", id, destination);
-                        pulse_queue.push_back((id.clone(), destination.clone(), Pulse::Low));
+                        pulse_queue.push_back(Pulse::new(PulseType::Low, id, destination));
                     }
                 }
             }
@@ -73,77 +84,52 @@ impl CommsModule {
                 destinations,
                 id,
             } => {
-                inputs.insert(source.to_string(), pulse.clone());
+                inputs.insert(pulse.source, pulse.pulse_type.clone());
 
-                if inputs.iter().all(|(_, pulse)| *pulse == Pulse::High) {
+                if inputs.iter().all(|(_, pulse)| *pulse == PulseType::High) {
                     for destination in destinations {
-                        //println!("{} -low-> {}", id, destination);
-                        pulse_queue.push_back((id.clone(), destination.clone(), Pulse::Low));
+                        pulse_queue.push_back(Pulse::new(PulseType::Low, id, destination));
                     }
                 } else {
                     for destination in destinations {
-                        //println!("{} -high-> {}", id, destination);
-                        pulse_queue.push_back((id.clone(), destination.clone(), Pulse::High));
+                        pulse_queue.push_back(Pulse::new(PulseType::High, id, destination));
                     }
                 }
             }
             Self::Broadcaster { destinations, id } => {
                 for destination in destinations {
-                    //println!(
-                    //    "{} -{}-> {}",
-                    //    id,
-                    //    match pulse {
-                    //        Pulse::High => "high",
-                    //        Pulse::Low => "low",
-                    //    },
-                    //    destination
-                    //);
-                    pulse_queue.push_back((id.clone(), destination.clone(), pulse.clone()));
+                    pulse_queue.push_back(Pulse::new(pulse.pulse_type.clone(), id, destination));
                 }
             }
         }
     }
 }
 
-fn parse_module(line: &str) -> (String, CommsModule) {
+fn parse_module(line: &str) -> (&str, CommsModule) {
     Regex::new(r"([%&])?([a-z]+) -> (.*)")
         .unwrap()
         .captures(line)
         .map(|captures| {
-            let id = captures.get(2).map(|m| m.as_str()).unwrap().to_string();
+            let id = captures.get(2).map(|m| m.as_str()).unwrap();
+            let destinations = captures
+                .get(3)
+                .map(|m| m.as_str())
+                .unwrap()
+                .split(", ")
+                .map(|s| s)
+                .collect();
             let module = match captures.get(1).map(|m| m.as_str()) {
                 Some("%") => CommsModule::FlipFlop {
                     on_off: false,
-                    destinations: captures
-                        .get(3)
-                        .map(|m| m.as_str())
-                        .unwrap()
-                        .split(", ")
-                        .map(|s| s.to_string())
-                        .collect(),
-                    id: id.clone(),
+                    destinations,
+                    id,
                 },
                 Some("&") => CommsModule::Conjunction {
                     inputs: HashMap::new(),
-                    destinations: captures
-                        .get(3)
-                        .map(|m| m.as_str())
-                        .unwrap()
-                        .split(", ")
-                        .map(|s| s.to_string())
-                        .collect(),
-                    id: id.clone(),
+                    destinations,
+                    id,
                 },
-                _ => CommsModule::Broadcaster {
-                    destinations: captures
-                        .get(3)
-                        .map(|m| m.as_str())
-                        .unwrap()
-                        .split(", ")
-                        .map(|s| s.to_string())
-                        .collect(),
-                    id: id.clone(),
-                },
+                _ => CommsModule::Broadcaster { destinations, id },
             };
 
             (id, module)
@@ -151,59 +137,7 @@ fn parse_module(line: &str) -> (String, CommsModule) {
         .unwrap()
 }
 
-fn press_button(modules: &mut HashMap<String, CommsModule>) -> (usize, usize) {
-    let mut signals =
-        VecDeque::from([("button".to_string(), "broadcaster".to_string(), Pulse::Low)]);
-
-    let mut low_counter = 0;
-    let mut high_counter = 0;
-
-    //println!("button -low-> broadcaster");
-
-    while !signals.is_empty() {
-        let (from, to, pulse) = signals.pop_front().unwrap();
-        if pulse == Pulse::Low {
-            low_counter += 1;
-        } else {
-            high_counter += 1;
-        }
-
-        if !modules.contains_key(to.as_str()) {
-            continue;
-        }
-
-        let module = modules.get_mut(to.as_str()).unwrap();
-
-        module.signal(&from, &mut signals, pulse);
-    }
-
-    (low_counter, high_counter)
-}
-
-fn press_button2(modules: &mut HashMap<String, CommsModule>, check: &str) -> bool {
-    let mut signals =
-        VecDeque::from([("button".to_string(), "broadcaster".to_string(), Pulse::Low)]);
-
-    while !signals.is_empty() {
-        let (from, to, pulse) = signals.pop_front().unwrap();
-
-        if from == check && to == "lx" && pulse == Pulse::High {
-            return true;
-        }
-
-        if !modules.contains_key(to.as_str()) {
-            continue;
-        }
-
-        let module = modules.get_mut(to.as_str()).unwrap();
-
-        module.signal(&from, &mut signals, pulse);
-    }
-
-    false
-}
-
-pub fn part_one(input: &str) -> Option<usize> {
+fn parse_input(input: &str) -> HashMap<&str, CommsModule> {
     let mut modules: HashMap<_, _> = input
         .lines()
         .map(|line| {
@@ -212,32 +146,84 @@ pub fn part_one(input: &str) -> Option<usize> {
         })
         .collect();
 
-    let mut conjunctions: Vec<(String, Vec<String>)> = Vec::new();
+    let mut conjunctions: Vec<(&str, Vec<&str>)> = Vec::new();
 
     for (id, module) in modules.iter() {
         if matches!(module, CommsModule::Conjunction { .. }) {
-            let inputs: Vec<String> = modules
+            let inputs: Vec<&str> = modules
                 .iter()
                 .filter(|(_, m)| m.has_destination(id))
-                .map(|(id, _)| id.clone())
+                .map(|(&id, _)| id)
                 .collect();
 
-            conjunctions.push((id.clone(), inputs));
+            conjunctions.push((id, inputs));
         }
     }
 
     for (id, new_inputs) in conjunctions {
         match modules.get_mut(&id).unwrap() {
             CommsModule::Conjunction { inputs, .. } => {
-                inputs.extend(
-                    new_inputs
-                        .iter()
-                        .map(|id: &String| (id.clone(), Pulse::Low)),
-                );
+                inputs.extend(new_inputs.iter().map(|&id| (id, PulseType::Low)));
             }
             _ => panic!("Expected conjunction"),
         }
     }
+
+    modules
+}
+
+fn press_button(modules: &mut HashMap<&str, CommsModule>) -> (usize, usize) {
+    let mut signals = VecDeque::from([Pulse::new(PulseType::Low, "button", "broadcaster")]);
+
+    let mut low_counter = 0;
+    let mut high_counter = 0;
+
+    while !signals.is_empty() {
+        let pulse = signals.pop_front().unwrap();
+
+        if pulse.pulse_type == PulseType::Low {
+            low_counter += 1;
+        } else {
+            high_counter += 1;
+        }
+
+        if !modules.contains_key(pulse.destination) {
+            continue;
+        }
+
+        let module = modules.get_mut(pulse.destination).unwrap();
+
+        module.signal(&mut signals, &pulse);
+    }
+
+    (low_counter, high_counter)
+}
+
+fn press_button2(modules: &mut HashMap<&str, CommsModule>, check: &str) -> bool {
+    let mut signals = VecDeque::from([Pulse::new(PulseType::Low, "button", "broadcaster")]);
+
+    while !signals.is_empty() {
+        let pulse = signals.pop_front().unwrap();
+
+        if pulse.source == check && pulse.destination == "lx" && pulse.pulse_type == PulseType::High
+        {
+            return true;
+        }
+
+        if !modules.contains_key(pulse.destination) {
+            continue;
+        }
+
+        let module = modules.get_mut(pulse.destination).unwrap();
+
+        module.signal(&mut signals, &pulse);
+    }
+
+    false
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut modules = parse_input(input);
 
     let mut lows = 0;
     let mut highs = 0;
@@ -246,48 +232,13 @@ pub fn part_one(input: &str) -> Option<usize> {
         let (low_count, high_count) = press_button(&mut modules);
         lows += low_count;
         highs += high_count;
-
-        //println!("");
     }
 
     Some(lows * highs)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut modules: HashMap<_, _> = input
-        .lines()
-        .map(|line| {
-            let (id, module) = parse_module(line);
-            (id, module)
-        })
-        .collect();
-
-    let mut conjunctions: Vec<(String, Vec<String>)> = Vec::new();
-
-    for (id, module) in modules.iter() {
-        if matches!(module, CommsModule::Conjunction { .. }) {
-            let inputs: Vec<String> = modules
-                .iter()
-                .filter(|(_, m)| m.has_destination(id))
-                .map(|(id, _)| id.clone())
-                .collect();
-
-            conjunctions.push((id.clone(), inputs));
-        }
-    }
-
-    for (id, new_inputs) in conjunctions {
-        match modules.get_mut(&id).unwrap() {
-            CommsModule::Conjunction { inputs, .. } => {
-                inputs.extend(
-                    new_inputs
-                        .iter()
-                        .map(|id: &String| (id.clone(), Pulse::Low)),
-                );
-            }
-            _ => panic!("Expected conjunction"),
-        }
-    }
+    let modules = parse_input(input);
 
     let tmp = ["cl", "rp", "lb", "nj"]
         .into_par_iter()
@@ -322,11 +273,5 @@ mod tests {
             "examples", DAY, 2,
         ));
         assert_eq!(result, Some(11_687_500));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 }
