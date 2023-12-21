d37398d153670daabe8e41cd16a77afd58b05421
Eric Barr <eb@eb2.co>
2023-12-21 11:55:07 -0700

    Day 21 part 1 solution

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,7 +1,87 @@
+use advent_of_code::Point2D;
+use itertools::Itertools;
+use std::collections::HashMap;
+
 advent_of_code::solution!(21);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+fn parse_map(input: &str) -> HashMap<Point2D, char> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, c)| (Point2D::new(x as isize, y as isize), c))
+        })
+        .collect()
+}
+
+fn find_reachable_points(map: HashMap<Point2D, char>, max_steps: usize) -> usize {
+    let start = map
+        .iter()
+        .find_map(|(pos, &c)| if c == 'S' { Some(pos) } else { None })
+        .unwrap();
+
+    let mut visited = vec![*start];
+    let mut queue = vec![*start];
+    let mut next_queue = vec![];
+
+    let mut steps = 0;
+
+    while steps < max_steps {
+        while let Some(pos) = queue.pop() {
+            let mut next = pos;
+            next.x += 1;
+            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next)
+            /* && !visited.contains(&next) */
+            {
+                next_queue.push(next);
+                visited.push(next);
+            }
+
+            let mut next = pos;
+            next.x -= 1;
+            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next)
+            /* && !visited.contains(&next) */
+            {
+                next_queue.push(next);
+                visited.push(next);
+            }
+
+            let mut next = pos;
+            next.y += 1;
+            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next)
+            /* && !visited.contains(&next) */
+            {
+                next_queue.push(next);
+                visited.push(next);
+            }
+
+            let mut next = pos;
+            next.y -= 1;
+            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next)
+            /* && !visited.contains(&next) */
+            {
+                next_queue.push(next);
+                visited.push(next);
+            }
+        }
+
+        queue = next_queue;
+        next_queue = vec![];
+
+        steps += 1;
+    }
+
+    dbg!(queue.iter().unique().count());
+
+    queue.iter().unique().count()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let map = parse_map(input);
+
+    Some(find_reachable_points(map, 64))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -14,8 +94,9 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let map = parse_map(&advent_of_code::template::read_file("examples", DAY));
+        let result = find_reachable_points(map, 6);
+        assert_eq!(result, 16);
     }
 
     #[test]
