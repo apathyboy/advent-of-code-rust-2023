a2f5fe6d6dfb3536bd3b62f78bb4b9fdb10c6fdb
eb <eb@eb2.co>
2023-12-22 15:12:55 -0700

    Day 21 refactor (broken)

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,6 +1,5 @@
 use advent_of_code::Point2D;
-use itertools::Itertools;
-use pathfinding::prelude::Matrix;
+use core::panic;
 use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(21);
@@ -17,7 +16,7 @@ fn parse_map(input: &str) -> HashMap<Point2D, char> {
         .collect()
 }
 
-fn find_reachable_points(map: HashMap<Point2D, char>, max_steps: usize) -> usize {
+fn find_reachable_points(map: &HashMap<Point2D, char>, max_steps: usize) -> usize {
     let start = map
         .iter()
         .find_map(|(pos, &c)| if c == 'S' { Some(pos) } else { None })
@@ -26,84 +25,75 @@ fn find_reachable_points(map: HashMap<Point2D, char>, max_steps: usize) -> usize
     let width = map.keys().map(|pos| pos.x).max().unwrap();
     let height = map.keys().map(|pos| pos.y).max().unwrap();
 
-    dbg!(width, height);
-
-    let mut queue = vec![*start];
-    let mut next_queue = vec![];
+    let mut queue = HashSet::from([*start]);
+    let mut next_queue = HashSet::new();
 
     let mut steps = 0;
 
     while steps < max_steps {
-        while let Some(pos) = queue.pop() {
-            let mut next = pos;
-            next.x = (next.x + 1).rem_euclid(width);
-            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next) {
-                next_queue.push(next);
-            }
+        for pos in queue.iter() {
+            for dir in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let mut next = *pos;
+                next.x = next.x + dir.0;
+                next.y = next.y + dir.1;
 
-            let mut next = pos;
-            next.x = (next.x - 1).rem_euclid(width);
-            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next) {
-                next_queue.push(next);
-            }
+                // In order to avoid copies, we can use a checkerboard pattern
+                //if steps as isize % 2 != (next.x + next.y).rem_euclid(2) {
+                //    continue;
+                //}
 
-            let mut next = pos;
-            next.y = (next.y + 1).rem_euclid(height);
-            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next) {
-                next_queue.push(next);
-            }
+                let mut check = next;
+                check.x = (check.x + (width * max_steps as isize)) % width;
+                check.y = (check.y + (height * max_steps as isize)) % height;
 
-            let mut next = pos;
-            next.y = (next.y - 1).rem_euclid(height);
-            if map.contains_key(&next) && map[&next] != '#' && !next_queue.contains(&next) {
-                next_queue.push(next);
+                //println!("Checking {:?} {:?}", next, check);
+                if map[&check] != '#' {
+                    next_queue.insert(next);
+                }
             }
         }
 
+        queue.clear();
         (queue, next_queue) = (next_queue, queue);
-        next_queue.clear();
 
         steps += 1;
+
+        println!("steps: {} {}", steps, queue.len());
+        // panic!();
+        //if ((steps as isize - (width / 2)) % 131) == 0 {
+        //    dbg!(steps, queue.len());
+        //}
     }
 
-    queue.iter().unique().count()
-}
-
-fn part(input: &str, goal: usize) -> usize {
-    let grid = Matrix::from_rows(input.lines().map(str::bytes)).unwrap();
-    let (sr, sc) = grid
-        .items()
-        .find_map(|(pos, b)| (*b == b'S').then_some(pos))
-        .unwrap();
-    let (g, mut ys, mut reachable) = (grid.rows, vec![], HashSet::new());
-    reachable.insert((sr as isize, sc as isize));
-    for count in 1..=goal {
-        for (r, c) in reachable.drain().collect::<Vec<_>>() {
-            reachable.extend(
-                [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
-                    .iter()
-                    .filter(|&&(nr, nc)| grid[grid.constrain((nr, nc))] != b'#'),
-            );
-        }
-        if count % g == g / 2 {
-            ys.push(reachable.len());
-            if let &[y0, y1, y2] = &ys[..] {
-                let x = goal / g;
-                return (x * x * (y0 + y2 - 2 * y1) + x * (4 * y1 - 3 * y0 - y2) + 2 * y0) / 2;
-            }
-        }
-    }
-    reachable.len()
+    queue.len()
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let map = parse_map(input);
 
-    Some(find_reachable_points(map, 64))
+    Some(find_reachable_points(&map, 64))
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    Some(part(input, 26501365))
+    let map = parse_map(input);
+
+    let y_0 = find_reachable_points(&map, 65);
+    let y_1 = find_reachable_points(&map, 65 + 131);
+    let y_2 = find_reachable_points(&map, 65 + 131 * 2);
+
+    let n = 202300;
+
+    let a2 = y_2 - 2 * y_1 + y_0;
+    let b2 = 4 * y_1 - 3 * y_0 - y_2;
+    let c = y_0;
+
+    println!("{a2}/2 x^2 +{b2}/2 x + {c} = y");
+    println!("x=0, y={c}");
+    println!("x=1, y={}", (a2 + b2) / 2 + c);
+    println!("x=2, y={}", (4 * a2 + 2 * b2) / 2 + c);
+    println!("x=202300, y={}", (n * n * a2 + n * b2) / 2 + c);
+
+    Some((n * n * a2 + n * b2) / 2 + c)
 }
 
 #[cfg(test)]
@@ -111,15 +101,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
+    fn test_find_reachable_points() {
         let map = parse_map(&advent_of_code::template::read_file("examples", DAY));
-        let result = find_reachable_points(map, 6);
-        assert_eq!(result, 16);
-    }
+        //let result = find_reachable_points(&map, 6);
+        //assert_eq!(result, 16);
 
-    #[test]
-    fn test_part_two() {
-        let result = part(&advent_of_code::template::read_file("examples", DAY), 10);
+        let result = find_reachable_points(&map, 102);
         assert_eq!(result, 50);
     }
 }
