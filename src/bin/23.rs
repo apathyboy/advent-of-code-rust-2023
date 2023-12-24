c93c58e5106095b2ae7e360db7e4c7f6a7738a6b
eb <eb@eb2.co>
2023-12-23 19:39:27 -0700

    Day 23 refactor

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -4,112 +4,33 @@ fn parse_map(input: &str) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn longest_path(maze: &Vec<Vec<char>>, x: usize, y: usize) -> Vec<(usize, usize)> {
-    let mut visited = vec![vec![false; maze[0].len()]; maze.len()];
-    let mut longest_path = Vec::new();
-    let mut current_path = Vec::new();
-
-    dfs1(
-        maze,
-        x,
-        y,
-        &mut visited,
-        &mut current_path,
-        &mut longest_path,
-    );
-
-    longest_path
+fn parse_map_no_slopes(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '<' | '>' | 'v' | '^' => '.',
+                    _ => c,
+                })
+                .collect()
+        })
+        .collect()
 }
 
-fn dfs1(
-    maze: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    visited: &mut Vec<Vec<bool>>,
-    current_path: &mut Vec<(usize, usize)>,
-    longest_path: &mut Vec<(usize, usize)>,
-) {
-    if x >= maze[0].len() || y >= maze.len() || maze[y][x] == '#' || visited[y][x] {
-        //println!("Invalid ({x}, {y})");
-        return;
-    }
-
-    //println!("Visiting ({x}, {y})");
-
-    visited[y][x] = true;
-    current_path.push((x, y));
-
-    if current_path.len() > longest_path.len() {
-        longest_path.clear();
-        longest_path.extend(current_path.iter());
-    }
-
-    // Explore neighbors (up, down, left, right)
-    let dx = [-1, 1, 0, 0];
-    let dy = [0, 0, -1, 1];
-
-    if maze[y][x] == '.' {
-        for i in 0..4 {
-            let new_x = x as i32 + dx[i];
-            let new_y = y as i32 + dy[i];
-
-            //println!("Checking ({new_x}, {new_y})");
-
-            if new_x >= 0 && new_x < maze[0].len() as i32 && new_y >= 0 && new_y < maze.len() as i32
-            {
-                dfs1(
-                    maze,
-                    new_x as usize,
-                    new_y as usize,
-                    visited,
-                    current_path,
-                    longest_path,
-                );
-            }
-        }
-    } else {
-        let slope_directions = vec![('<', (-1, 0)), ('^', (0, -1)), ('>', (1, 0)), ('v', (0, 1))];
-
-        for (slope_char, (dx, dy)) in &slope_directions {
-            if maze[y][x] == *slope_char {
-                let new_x = x as i32 + dx;
-                let new_y = y as i32 + dy;
-
-                if new_x >= 0
-                    && new_x < maze[0].len() as i32
-                    && new_y >= 0
-                    && new_y < maze.len() as i32
-                {
-                    dfs1(
-                        maze,
-                        new_x as usize,
-                        new_y as usize,
-                        visited,
-                        current_path,
-                        longest_path,
-                    );
-                }
-            }
-        }
-    }
-
-    visited[y][x] = false;
-    current_path.pop();
-}
-
-fn longest_path2(
+fn longest_path(
     maze: &Vec<Vec<char>>,
     start: (usize, usize),
-    end: (usize, usize),
+    goal: (usize, usize),
 ) -> Vec<(usize, usize)> {
     let mut visited = vec![vec![false; maze[0].len()]; maze.len()];
     let mut longest_path = Vec::new();
     let mut current_path = Vec::new();
 
-    dfs2(
+    dfs(
         maze,
         start,
-        end,
+        goal,
         &mut visited,
         &mut current_path,
         &mut longest_path,
@@ -118,10 +39,10 @@ fn longest_path2(
     longest_path
 }
 
-fn dfs2(
+fn dfs(
     maze: &Vec<Vec<char>>,
     pos: (usize, usize),
-    end: (usize, usize),
+    goal: (usize, usize),
     visited: &mut Vec<Vec<bool>>,
     current_path: &mut Vec<(usize, usize)>,
     longest_path: &mut Vec<(usize, usize)>,
@@ -131,35 +52,35 @@ fn dfs2(
         || maze[pos.1][pos.0] == '#'
         || visited[pos.1][pos.0]
     {
-        //println!("Invalid ({x}, {y})");
         return;
     }
-
-    //println!("Visiting ({x}, {y})");
 
     visited[pos.1][pos.0] = true;
     current_path.push((pos.0, pos.1));
 
-    if pos.0 == end.0 && pos.1 == end.1 && current_path.len() > longest_path.len() {
+    if pos == goal && current_path.len() > longest_path.len() {
         longest_path.clear();
         longest_path.extend(current_path.iter());
-
-        //println!("New longest path: {:?}", longest_path.len() - 1);
     }
 
-    // Explore neighbors (up, down, left, right)
-    let dx = [-1, 1, 0, 0];
-    let dy = [0, 0, -1, 1];
+    let check_directions = match maze[pos.1][pos.0] {
+        '.' => vec![(-1, 0), (1, 0), (0, -1), (0, 1)],
+        '<' => vec![(-1, 0)],
+        '^' => vec![(0, -1)],
+        '>' => vec![(1, 0)],
+        'v' => vec![(0, 1)],
+        _ => unreachable!(),
+    };
 
-    for i in 0..4 {
-        let new_x = pos.0 as i32 + dx[i];
-        let new_y = pos.1 as i32 + dy[i];
+    for (dx, dy) in &check_directions {
+        let new_x = pos.0 as i32 + dx;
+        let new_y = pos.1 as i32 + dy;
 
         if new_x >= 0 && new_x < maze[0].len() as i32 && new_y >= 0 && new_y < maze.len() as i32 {
-            dfs2(
+            dfs(
                 maze,
                 (new_x as usize, new_y as usize),
-                end,
+                goal,
                 visited,
                 current_path,
                 longest_path,
@@ -174,36 +95,24 @@ fn dfs2(
 pub fn part_one(input: &str) -> Option<u32> {
     let map = parse_map(input);
 
-    let longest_path = longest_path(&map, 1, 0);
+    let start = (1, 0);
+    let end = (map[0].len() - 2, map.len() - 1);
 
+    let longest_path = longest_path(&map, start, end);
+
+    // steps taken is 1 less than the number of tiles visited
     Some(longest_path.len() as u32 - 1)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let map = parse_map(input);
+    let map = parse_map_no_slopes(input);
 
     let start = (1, 0);
     let end = (map[0].len() - 2, map.len() - 1);
 
-    let longest_path = longest_path2(&map, start, end);
+    let longest_path = longest_path(&map, start, end);
 
-    //println!();
-    //
-    //for y in 0..map.len() {
-    //    for x in 0..map[0].len() {
-    //        if longest_path.contains(&(x, y)) {
-    //            print!("O");
-    //        } else {
-    //            print!("{}", map[y][x]);
-    //        }
-    //    }
-    //    println!();
-    //}
-    //
-    //println!();
-    //println!("{:?}", longest_path.len());
-    //println!("{:?}", longest_path.iter().unique().count());
-
+    // steps taken is 1 less than the number of tiles visited
     Some(longest_path.len() as u32 - 1)
 }
 
